a8961afd397746996478ecbed82940a703ae5e2f
onturaa <174931989+onturaa@users.noreply.github.com>
2025-08-16 15:00:22 -0400

    Update to use on web
    use window.startGeneration = function... instead of the regular function

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let secondPasswordEl = document.getElementById("second-pass-el")
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-function generatePassword(length) {
+window.generatePassword = function generatePassword(length) {
     let randomPassword = ""
     for (let i = 0; i < length; i++) {
         let randomIndex = Math.floor(Math.random() * characters.length)
@@ -13,7 +13,7 @@ function generatePassword(length) {
     return randomPassword
 }
 
-function startGeneration() {
+window.startGeneration = function startGeneration() {
     let input = +document.getElementById("user-input").value
     if (!input || input < 8) {
         input = 15
